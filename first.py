7503c6d96dbe989f6d7cd23cb2a9662f755d9f47
Derek Welsh <20361721+dwelsh22@users.noreply.github.com>
2019-04-09 12:31:56 -0700

    Added .scr file content to for loops

--- first.py
+++ first.py
@@ -17,7 +17,11 @@ with open('test.csv', "r", newline='' ) as csvfile:
 print(DrawingList)
 
 newScr = open("new_script_file.scr", "w")
+newScr.write("filedia 0\n")
 for drawing in DrawingList:
+    newScr.write("open ~\" + drawing[Project_Number] + "-" + drawing[Drawing_Number] + ".dwg\n")
+    newScr.write("-EDITBLOCKATTRIBUTE\n\n") 
     for key, value in drawing.items():
-        newScr.write("Fix the parameter " + key + " to " + value + "\n")
-    newScr.write("\n")
+        newScr.write("TitleInfo\n*\n\n0,0\n300,50\n\nV\nR\n" + "dictionary key and value") 
+        #newScr.write("Fix the parameter " + key + " to " + value + "\n")
+    newScr.write("\nclose")
